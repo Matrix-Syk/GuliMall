ce4604f591404b9e0485018f86960d2bca5a6428
sykun <sykun321@163.com>
2023-04-08 21:50:55 +0800

    nacos注册成功

--- gulimall-coupon/src/main/java/com/matrix/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/matrix/gulimall/coupon/GulimallCouponApplication.java
@@ -1,9 +1,13 @@
 package com.matrix.gulimall.coupon;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@MapperScan("com.matrix.gulimall.coupon.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GulimallCouponApplication {
 
     public static void main(String[] args) {
